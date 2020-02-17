a6ab7a151909a80f42edc483b1e90a4ad5a4397b
Ben Woodard <benrwoodard@gmail.com>
2020-02-17 08:08:03 -0500

    added starttime to the function call defined attributes added started to help include time that may need to be started earlier.

--- R/starttimer.R
+++ R/starttimer.R
@@ -8,11 +8,19 @@
 #' @description Use a project name that you can reference.
 #'
 #' @import Tidyverse
-#'
+#' @param pn Project Name
+#' @param starttime The time that the project started. If you started
+#' earlier and want to catch up add
+#' @param started The time delay in decimal hour when you started the project.
+#' For example use '.50' to start the project 30 minutes ago.
 #' @include
 
-start <- function(pn) {
-  starttime <- Sys.time()
+start <- function(pn, starttime = Sys.time(), started = NA) {
+  if(is.na(started)) {
+    started = started*60*60
+    starttime = starttime - started
+  }
+
   p <- data.frame(pn, starttime)
   assign("ps", p, envir = .GlobalEnv)
 }
