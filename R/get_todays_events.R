56ce2f78a108c4cb4a2c1cf1c411c0ec22cd6499
Ben Woodard <benrwoodard@gmail.com>
2020-02-25 16:54:47 -0500

    easier query to pull in the list of events for today

---
+++ R/get_todays_events.R
@@ -0,0 +1,20 @@
+#' Get list of the events scheduled for today (or any day).
+#'
+#'
+#' @param token token obtained from [calendar_token()]
+#'
+#' @return Tibble with columns for .
+#' @family calendar functions
+#'
+#' @export
+
+gcaltoday <- function(today = Sys.Date(), id = "ben.woodard@searchdiscovery.com") {
+
+today <- calendar_events(id = id ,now = today, days_in_future = 1,days_in_past = 0)
+events <- today %>%
+  mutate(start_time = format(start_datetime-(5*60*60), "%H:%M"),
+         end_time = format(end_datetime-(5*60*60), "%H:%M") )  %>%
+  select(start_time, end_time, summary, description) %>%
+  arrange(start_time)
+return(events)
+}
