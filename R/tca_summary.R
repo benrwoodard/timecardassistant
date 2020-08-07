234cbdc01807ea8fe0d642e5af1363f37fd02a98
Ben Woodard <ben.woodard@searchdiscovery.com>
2020-08-06 21:41:45 -0400

    changing the summary date to function properely

--- R/tca_summary.R
+++ R/tca_summary.R
@@ -13,13 +13,13 @@
 
 tca_summary <- function(timecard = timecard, date_filter = Sys.Date()) {
 
-  summary <- timecard %>%
+  timecard %>%
     filter(date == date_filter) %>%
     mutate(client = tolower(client)) %>%
     group_by(date, client) %>%
     summarise(rawtime = sum(dif), psatime = sum(psatime))
 
-  summary
+
 
 }
 
