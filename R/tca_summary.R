9fdbfccf043866af005e16bbf4a2c7220d77161e
Ben Woodard <ben.woodard@searchdiscovery.com>
2020-09-03 05:35:29 -0400

    added a new tca_summary feature that pulls in the first date for the current week and then spreads the data to look more like what you see in PSA

--- R/tca_summary.R
+++ R/tca_summary.R
@@ -7,21 +7,20 @@
 #' @param timecard adds the timecard dataframe to the function
 #' @param date_filter This helps filter the results to today's date or change it.
 #'
-#' @import dplyr
+#' @import dplyr lubridate
 #'
 #' @export
 
-tca_summary <- function(timecard = timecard, date_filter = Sys.Date()) {
+tca_summary <- function(timecard = timecard, date_filter = floor_date(as.Date(Sys.Date(), "%m/%d/%Y"), unit="week")) {
 
   timecard %>%
-    filter(date == date_filter) %>%
+    filter(date >= date_filter) %>%
     mutate(client = tolower(client)) %>%
-    group_by(client) %>%
-    summarise(psatime = sum(psatime)) %>%
-    ungroup() %>%
-    add_row(summarise_all(., funs(if(is.numeric(.)) sum(.) else "Total")))
-
-
+    group_by(date, client) %>%
+    summarise( psatime = sum(psatime)) %>%
+    arrange(client) %>%
+    spread(date, psatime)%>%
+    add_row(summarise_all(., funs(if(is.numeric(.)) sum(., na.rm = T) else "Total")))
 
 }
 
